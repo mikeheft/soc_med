ad67db6091615146c57cec090c6b964ce88e0e7c
Mike Heft <mikeheft@gmail.com>
2020-06-06 14:30:31 -0600

    Fix typo in require

--- app/models/soc_med/like.rb
+++ app/models/soc_med/like.rb
@@ -1,5 +1,4 @@
-require '../../../app/models/soc_med/concerns/soc_med_helper'
-
+require 'concerns/soc_med_helper'
 module SocMed
   class Like < ::ApplicationRecord
     include SocMed::SocMedHelper
