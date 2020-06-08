099ff7e4d5b276cab77d4dba06b97b34ecd7f9f3
Mike Heft <mikeheft@gmail.com>
2020-06-08 15:01:05 -0600

    Add error reponse method

--- app/controllers/soc_med/application_controller.rb
+++ app/controllers/soc_med/application_controller.rb
@@ -5,5 +5,9 @@ module SocMed
     include DrySerialization::Blueprinter
     protect_from_forgery unless: -> { request.format.json? }
     # protect_from_forgery with: :exception
+
+    def error_response(error)
+      render json: { status: error.code, message: error.message }
+    end
   end
 end
--- app/controllers/soc_med/likes_controller.rb
+++ app/controllers/soc_med/likes_controller.rb
@@ -6,7 +6,7 @@ module SocMed
     def create
       like_service::Create.call(params) do |success, failure|
         success.call {|object| render json: { like: serialized_resource(object, ::Blueprints::Likes::OverviewBlueprint, view: :extended) } }
-        failure.call(&method(:error))
+        failure.call(&method(:error_reponse))
       end
     end
 
