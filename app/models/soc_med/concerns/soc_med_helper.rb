ba09fc4843a067b94d5b14b140ee352506f3581b
Mike Heft <mikeheft@gmail.com>
2020-06-08 15:25:53 -0600

    Work on inclusions

--- app/models/soc_med/concerns/soc_med_helper.rb
+++ app/models/soc_med/concerns/soc_med_helper.rb
@@ -1,15 +1,17 @@
 module SocMed
-  module SocMedHelper
+  module Concerns
+    module SocMedHelper
 
-    def raise_not_implemented_error_if_required
-      raise NotImplementedError unless target.respond_to?(:number_of_follows)
+      def raise_not_implemented_error_if_required
+        raise NotImplementedError unless target.respond_to?(:number_of_follows)
+      end
+
+      def update_count(attribute, operator)
+        count = target.send(attribute).send(operator, 1)
+
+        target.update("#{attribute}" => count)
+      end
+
     end
-
-    def update_count(attribute, operator)
-      count = target.send(attribute).send(operator, 1)
-
-      target.update("#{attribute}" => count)
-    end
-
   end
 end
