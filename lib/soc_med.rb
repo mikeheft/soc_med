745075248eaf654711dd56e960ab1cd75a87e5f3
Mike Heft <mikeheft@gmail.com>
2020-06-08 15:24:34 -0600

    Add require statements

--- lib/soc_med.rb
+++ lib/soc_med.rb
@@ -1,4 +1,16 @@
 require "soc_med/engine"
+require 'soc_med/concerns/likeable'
+require 'soc_med/concerns/followable'
+require 'soc_med/concerns/blockable'
+require 'soc_med/concerns/reportable'
+require 'soc_med/services/likes/create'
+require 'soc_med/services/likes/destroy'
+require 'soc_med/services/follows/create'
+require 'soc_med/services/follows/destroy'
+require 'soc_med/services/reports/create'
+require 'soc_med/services/reports/destroy'
+require 'soc_med/services/blocks/create'
+require 'soc_med/services/blocks/destroy'
 
 module SocMed
   mattr_accessor :owner_class
